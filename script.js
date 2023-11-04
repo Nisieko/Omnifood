45f8d02687ed6cb5a6dbde14662cd58fe2e90faf
AveryL <lxylbznb@gmail.com>
2023-11-04 20:45:21 +0000

    Upload

---
+++ script.js
@@ -0,0 +1,51 @@
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    //Scrollback to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+/********************************/
+/* Sticky Navigation*/
+/********************************/
+const secHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    } else {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    //In the viewport
+    root: null,
+    threshold: 0, //get an event when the section observed moved out of the section
+    rootMargin: "-80px",
+  }
+);
+
+obs.observe(secHeroEl);
